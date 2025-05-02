50735a2b344bdfb8be431a4132ea98642ad53a58
ed3t <ever.asap@gmail.com>
2025-05-02 09:48:08 +0100

    Chore: add support for https

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -8,4 +8,7 @@ export const config = {
   environment: process.env.NODE_ENV ?? 'local',
   frontendUrl: process.env.FRONTEND_URL,
   frontendDomain: process.env.FRONTEND_DOMAIN,
+  https: process.env.HTTPS === 'true',
+  sslKey: process.env.SSL_KEY_PATH || './certs/key.pem',
+  sslCert: process.env.SSL_CERT_PATH || './certs/cert.pem',
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,17 +9,29 @@ moduleAlias.addAliases({
 import fs from 'fs';
 import { AddressInfo } from 'net';
 import http from 'http';
+import https from 'https';
 import { config } from '@config/config';
 import { createServer } from '@config/express';
 
 async function startServer() {
   const app = createServer();
-  const server = http.createServer(app).listen({host, port}, () => {
-    const addressInfo = server.address() as AddressInfo;
-    logger.info(
-      `Server ready at http://${addressInfo.address}:${addressInfo.port}`,
-    );
-  });
+  let server: http.Server | https.Server;
+  if (config.https && config.sslKey && config.sslCert) {
+    const sslOptions = {
+      key: fs.readFileSync(config.sslKey),
+      cert: fs.readFileSync(config.sslCert),
+    };
+
+    server = https.createServer(sslOptions, app).listen({ host: config.host, port: config.port }, () => {
+      const addressInfo = server.address() as AddressInfo;
+      console.log(`https server ready at https://${addressInfo.address}:${addressInfo.port}`);
+    });
+  } else {
+    server = http.createServer(app).listen({ host: config.host, port: config.port }, () => {
+      const addressInfo = server.address() as AddressInfo;
+      console.log(`http server ready at http://${addressInfo.address}:${addressInfo.port}`);
+    });
+  }
 
   const signalTraps: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
   signalTraps.forEach((type) => {

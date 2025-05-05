6776746f955266b1418d202bbeb6870c14942aa9
ed3t <ever.asap@gmail.com>
2025-05-05 10:38:44 +0100

    Modify date string helper

--- frontend/src/components/commons/ListItem.jsx
+++ frontend/src/components/commons/ListItem.jsx
@@ -1,26 +1,15 @@
 import React from "react";
+import { formatDate } from "@app/helpers";
 
 export default function ListItem({ urlObject }) {
   const shortUrl = `${import.meta.env.VITE_FRONTEND_URL}/${urlObject?.shortUrlPath}`;
 
-  const formattedDate = new Date(urlObject?.createdAt).toLocaleDateString(
-    undefined,
-    {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    }
-  );
-
-  const formattedTime = new Date(urlObject?.createdAt).toLocaleTimeString(undefined, {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  const { formattedDate, formattedTime } = formatDate(urlObject?.createdAt);
 
   return (
     <div className="flex-table-item is-order">
-      <div className="flex-table-cell" data-th="Long Url">
-        <span>{urlObject?.longUrl}</span>
+      <div className="flex-table-cell is-grow" data-th="Long Url">
+        <span className="long-url is-list">{urlObject?.longUrl}</span>
       </div>
       <div className="flex-table-cell is-grow" data-th="Short Url">
         <span>
@@ -34,7 +23,7 @@ export default function ListItem({ urlObject }) {
           </a>
         </span>
       </div>
-      <div className="flex-table-cell is-grow" data-th="Visits">
+      <div className="flex-table-cell" data-th="Visits">
         <span className="light-text">{urlObject?.visits}</span>
       </div>
       <div className="flex-table-cell cell-end" data-th="Created">
